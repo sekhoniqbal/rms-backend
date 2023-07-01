03f1a17e3f15aa1ed264e28799a74809b627f494
iqbal singh <iqbal.singh@ehealthsask.ca>
2023-06-30 18:29:06 -0600

    added validation constrain to allow name between 2 to 30 characters

--- src/main/java/ca/ehealthsask/rmsdemo/dtos/PatientDto.java
+++ src/main/java/ca/ehealthsask/rmsdemo/dtos/PatientDto.java
@@ -1,5 +1,7 @@
 package ca.ehealthsask.rmsdemo.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -7,5 +9,6 @@ import lombok.Getter;
 public class PatientDto {
 
     @NotBlank(message = "Name cannot be blank")
+    @Length(min = 2, max = 30, message = "Name can only be 2 to 30 characters long")
     private String name;
 }
