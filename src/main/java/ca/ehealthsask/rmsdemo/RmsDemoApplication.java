a67eaa1bd8c4b16c156c97b2529d07e9bf83ce2d
iqbal singh <iqbal.singh@ehealthsask.ca>
2023-06-30 18:23:34 -0600

    allowed cross origin  access

--- src/main/java/ca/ehealthsask/rmsdemo/RmsDemoApplication.java
+++ src/main/java/ca/ehealthsask/rmsdemo/RmsDemoApplication.java
@@ -1,12 +1,30 @@
 package ca.ehealthsask.rmsdemo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RmsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RmsDemoApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(false);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://hoppscotch.io/", "*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
